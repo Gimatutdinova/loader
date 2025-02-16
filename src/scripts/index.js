23c55374a90e31f64fb2ffa2b6fd4c42c17040d5
kamila <gimatutdinova.k@gmail.com>
2025-02-16 13:17:22 +0300

    init project

---
+++ src/scripts/index.js
@@ -0,0 +1,3 @@
+import "Styles/style.sass";
+
+console.log('index.js')
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+      filename: 'main.js',
+  },
+  resolve: {
+    alias: {
+      Styles: path.resolve(__dirname, 'src/styles/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss|sass)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+  },
+  mode: 'development',
+};
